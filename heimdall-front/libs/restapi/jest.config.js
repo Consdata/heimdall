5749fc377f1a6a83b68cb859eccb1ae726971291
dmejer <dmejer@consdata.pl>
2019-05-28 10:02:12 +0200

    Revert "nx restapi"
    This reverts commit 0d166d5e

--- heimdall-front/libs/restapi/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'restapi',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/restapi',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
