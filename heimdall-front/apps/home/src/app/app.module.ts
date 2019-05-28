4642c8d3736007fdf446e7ad9c0546f8af999cdc
dmejer <dmejer@consdata.pl>
2019-05-28 10:35:04 +0200

    nx rest-api info rest service

--- heimdall-front/apps/home/src/app/app.module.ts
+++ heimdall-front/apps/home/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {RestApiModule} from '@heimdall-front/rest-api';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RestApiModule,
+    RouterModule.forRoot([], {initialNavigation: 'enabled'})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
