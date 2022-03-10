3087bc3e57e93809707050844f07404cf958849e
mchrapkowicz <mchrapkowicz@consdata.pl>
2022-03-10 10:35:49 +0100

    ICL-5515 dummy table

---
+++ frontend/consdata/libs/feature-dependency-table-cell/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'feature-dependency-table-cell',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/feature-dependency-table-cell',
+};
